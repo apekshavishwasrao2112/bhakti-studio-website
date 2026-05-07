9cfc474a28d0c496ab96b4304201bed48fc4e49b
Apeksha Ramesh Vishwasrao <apekshavishwasrao2112@gmail.com>
2026-05-07 10:55:53 +0530

    mysql added3

--- app.py
+++ app.py
@@ -493,7 +493,5 @@ def chat():
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
-   app.run(
-    host='0.0.0.0',
-    port=int(os.getenv("PORT", 5000))
-)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
